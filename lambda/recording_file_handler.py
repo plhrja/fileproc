4de6d88a2b7600f7548c739c0ab0e02da9c4f8f2
plhrja <robert.sirvio@alumni.helsinki.fi>
2025-01-08 15:35:18 +0200

    Process record
    Requires still some data processing - Firehose would just combo input data as is resulting in multiple arrays per file

--- lambda/recording_file_handler.py
+++ lambda/recording_file_handler.py
@@ -12,26 +12,22 @@ s3 = boto3.client("s3")
 table_name = os.environ["DYNAMO_TABLE_NAME"]
 table = dynamodb.Table(table_name)
 
-def handler(event: events.S3Event):
-    print("Received event:", json.dumps(event, indent=2))
-    for record in event["detail"]["records"]:
-        try:
-            bucket_name = record["s3"]["bucket"]["name"]
-            object_key = unquote_plus(record["s3"]["object"]["key"])
-            
-            # Fetch file metadata
-            response = s3.head_object(Bucket=bucket_name, Key=object_key)
-            metadata = {
-                "file_id": object_key,
-                "bucket_name": bucket_name,
-                "size": response["ContentLength"],
-                "last_modified": response["LastModified"].isoformat(),
-                "content_type": response["ContentType"]
-            }
-            
-            # Insert into DynamoDB
-            table.put_item(Item=metadata)
-            print(f"Successfully processed {object_key}")
-        except Exception as e:
-            print(f"Error processing file {record}: {e}")
-            raise
+def handler(event: events.EventBridgeEvent, context):
+    print("Received event:", json.dumps(event))
+    try:
+        bucket_name = event["detail"]["bucket"]["name"]
+        object_key = unquote_plus(event["detail"]["object"]["key"])
+        
+        # Fetch file content
+        s3_object = s3.get_object(Bucket=bucket_name, Key=object_key)
+        file_content = s3_object['Body'].read().decode('utf-8')
+        data = json.loads(file_content, strict=False)
+        
+        with table.batch_writer() as batch:
+            for item in data:
+                batch.put_item(Item=item)
+
+        print(f"Successfully processed {object_key}")
+    except Exception as e:
+        print(f"Error processing event {event}: {e}")
+        raise
